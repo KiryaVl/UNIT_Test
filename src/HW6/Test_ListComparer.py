b1a59465a3d6fec515e0860f6145662e2b0b48a2
KiryaVl <rabbitnya@gmail.com>
2023-10-29 11:42:21 +0300

    HW6

--- src/HW6/Test_ListComparer.py
+++ src/HW6/Test_ListComparer.py
@@ -1,26 +1,26 @@
 import pytest
-from List_Comparer import List_Comparer
+from ListComparer import ListComparer
 
 
 def test_compare_lists_list1_greater():
-    comparer = List_Comparer([4, 5, 6], [1, 2, 3])
+    comparer = ListComparer([4, 5, 6], [1, 2, 3])
     result = comparer.compare_lists()
     assert result == "Первый список имеет большее среднее значение"
 
 
 def test_compare_lists_list2_greater():
-    comparer = List_Comparer([1, 2, 3], [4, 5, 6])
+    comparer = ListComparer([1, 2, 3], [4, 5, 6])
     result = comparer.compare_lists()
     assert result == "Второй список имеет большее среднее значение"
 
 
 def test_compare_lists_equal_average():
-    comparer = List_Comparer([1, 2, 3], [1, 2, 3])
+    comparer = ListComparer([1, 2, 3], [1, 2, 3])
     result = comparer.compare_lists()
     assert result == "Средние значения равны"
 
 
 def test_compare_lists_empty_lists():
-    comparer = List_Comparer([], [])
+    comparer = ListComparer([], [])
     result = comparer.compare_lists()
     assert result == "Средние значения равны"
